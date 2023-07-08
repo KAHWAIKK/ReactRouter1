40eefe9e58a8fcb559197e8f5326e11d37e36c62
iamKK <kahwaikagwima@gmail.com>
2023-07-08 20:05:56 +0300

    Added useLoaderData hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { 
+/* import { 
   Route,
   RouterProvider,
   createBrowserRouter,
@@ -54,7 +54,63 @@ function App() {
 }
 
 export default App;
+ */
 
+
+
+
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home'
+import About from './pages/About'
+import Faq from './pages/Help/Faq'
+import Contact from './pages/Help/Contact'
+import NotFound from './pages/NotFound'
+import Careers from './pages/careers/Careers'
+import careersloader  from './pages/careers/Careers'
+
+// layouts
+import RouteLayout from './Layout/RouteLayout'
+import HelpLayout from './Layout/HelpLayout'
+import CareersLayout from './Layout/CareersLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RouteLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact/>} />
+      </Route>
+
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route 
+          index 
+          element={<Careers />} 
+          loader={careersloader} 
+        />
+      </Route>
+
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App
 
 /* Normally when you click a link on anormal html a tag it sends a request to the server which then sends back the info requested, however react has the router compompontnt which intercepts this request and instead handles this within the brwoser, it does this using {Links} and (NavLink) component inside of the Routes component 
 
--- src/pages/careers/Careers.js
+++ src/pages/careers/Careers.js
@@ -1,17 +1,26 @@
-import React from 'react'
-import {useLoaderData,Link} from 'react'
+/* import React from 'react'
+import {Link} from 'react-router-dom'
+import {useLoaderData} from 'react-router-dom'
+ */
 
+/* loader function */
+/* export const careersloader = async ()=>{
+    const res = await fetch('http://localhost:5000/careers')
 
+   return res.json()
+}
 
-const Careers = () => {
+const Careers = ()=>{ */
     /* after the careersLoader function is done fetching the data via the endpoint it will return data in json format, we will then use a hook called useLoaderData which will return the data back tpo us*/
-    const careers = useLoaderData()
+   /*  const resp = useLoaderData()
+
+
   return (
     <div className="careers">
-        {careers.map((career)=>(
-            <Link to="/" key={career.id}>
-                <p>{career.title}</p>
-                <p>Based in {career.location}</p>
+        {resp.map(n=>(
+            <Link to="/" key={n.id}>
+                <p>{n.title}</p>
+                <p>Based in {n.location}</p>
             </Link>
         ))
         }
@@ -24,9 +33,28 @@ const Careers = () => {
 export default Careers
 
 
-/* loader function */
-export const careersloader = async ()=>{
-    const res = await fetch('http://localhost:4000/careers')
+ */
 
-    return res.json()
+import { Link, useLoaderData } from "react-router-dom"
+
+export default function Careers() {
+  const careers = useLoaderData()
+
+  return (
+    <div className="careers">
+      {careers.map(career => (
+        <Link to='/' key={career.id}>
+          <p>{career.title}</p>
+          <p>Based in {career.location}</p>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+// data loader
+export const careersloader = async () => {
+  const res = await fetch('http://localhost:5000/careers') 
+
+  return res.json()
 }
